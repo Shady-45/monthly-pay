390bdbc61a2f2b23b83c58f89cc2cadae2b75eda
Rahul <rahulchalat@yahoo.com>
2022-06-26 22:05:25 +0530

    new

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,34 +7,61 @@ const Header = () => {
       <nav>
         <div className="left">
           <p>
-            Monthly <span>pay</span>
+            <>Monthly</> <span className="monthly-new">Pay</span>
+            {/* <img className="rbus" src="/rbus.png" alt="" /> */}
           </p>
         </div>
         <div className="right">
-          <span>About</span>
-          <span>Blog</span>
-          <span>Monthly Baskets</span>
-          <span>Contact</span>
-          <button>Get your monthly card</button>
+          <span className="item-2">About</span>
+          <span className="item-3">Blog</span>
+          <span className="item-4">Monthly Baskets</span>
+          <span className="item-5">Contact</span>
+          <button>Get your Monthly Card</button>
         </div>
       </nav>
       <header>
         <div class="container header__container">
           <div class="header__left">
-            <h1>India's first Neo bank for Monthly Payments </h1>
-            <p>Saving from Spendings</p>
+            <h1>
+              India's first Neo bank for{" "}
+              <span style={{ color: "rgb(0, 184, 153)" }}>Monthly</span>{" "}
+              Payments{" "}
+            </h1>
+            <p>(Savings from Spendings)</p>
           </div>
 
           <div class="header__right">
             <div class="header__right-image">
-              <video autoPlay muted loop src="/video1.mp4"></video>
+              <img
+                src="https://media3.giphy.com/media/NkQu1zXYW7gmlCFHXb/giphy.gif?cid=790b76117b97fd3ceda55a2e33f9d3d9f77fcf0db58dbbde&rid=giphy.gif&ct=g"
+                alt=""
+              />
             </div>
           </div>
         </div>
       </header>
-      <footer>
-        <div className="container-1"></div>
-      </footer>
+
+      {/*   <footer>
+        <div className="container-1">
+          <div className="paragraph">
+            <p className="para">
+              Download <span className="monthly">Monthly</span> Pe Now
+            </p>
+            <a href="#">
+              <img className="store" src="/google.png" alt="" />
+
+              <img className="store-apple" src="/appple.png" alt="" />
+            </a>
+          </div>
+        </div>
+      </footer> */}
+
+      <div className="container-new">
+        <div className="image">
+          {/* <img src="/credit-new" alt="" /> */}
+          <video autoPlay loop muted src="/video-new.mp4"></video>
+        </div>
+      </div>
     </div>
   );
 };
